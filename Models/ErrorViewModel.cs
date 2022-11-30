fda9f578209a5f64678aa167747671c47f302d2f
Bearium-BB <brett200410@gmail.com>
2022-11-30 11:14:31 -0600

    git int

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Models_Exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
