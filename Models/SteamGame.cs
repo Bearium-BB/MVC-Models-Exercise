c213a95660f74fe4d0b088f8830478c8f6952f1e
Bearium-BB <brett200410@gmail.com>
2022-12-02 00:01:26 -0600

    Got more pacific information on game

--- Models/SteamGame.cs
+++ Models/SteamGame.cs
@@ -2,9 +2,11 @@
 using Steam.Models;
 using Steam.Models.DOTA2;
 using Steam.Models.SteamCommunity;
+using Steam.Models.SteamStore;
 using SteamWebAPI2.Interfaces;
 using SteamWebAPI2.Utilities;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text.Json;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -12,22 +14,19 @@ namespace MVC_Models_Exercise.Models
 {
     public class SteamGame
     {
-        public List<SteamAppModel> FriendsList { get; set; }
+        public StoreAppDetailsDataModel Game { get; set; }
         private SteamWebInterfaceFactory webInterfaceFactory = new SteamWebInterfaceFactory("49C1D18FEEABD048FF28292E62231812");
 
 
 
         public SteamGame()
         {
-            var eggsTask = testAsyncGame();
-            List<SteamAppModel> friendModels = eggsTask.Result;
-            FriendsList = friendModels;
+            var gameTask = testAsyncGame();
+            var game = gameTask.Result;
+            Game = game;
         }
         private async Task<List<FriendModel>> testAsync()
         {
-            // factory to be used to generate various web interfaces
-            // this will map to the ISteamUser endpoint
-            // note that you have full control over HttpClient lifecycle here
             var steamInterface = webInterfaceFactory.CreateSteamWebInterface<SteamUser>(new HttpClient());
             Task.Delay(3000).Wait();
             var friendsListResponse = await steamInterface.GetFriendsListAsync(76561198203306902);
@@ -35,17 +34,12 @@ namespace MVC_Models_Exercise.Models
             return friendsList.ToList();
         }
 
-        private async Task<List<SteamAppModel>> testAsyncGame()
+        private async Task<StoreAppDetailsDataModel> testAsyncGame()
         {
-            // factory to be used to generate various web interfaces
-            // this will map to the ISteamUser endpoint
-            // note that you have full control over HttpClient lifecycle here
-            var steamInterface = webInterfaceFactory.CreateSteamWebInterface<SteamApps>(new HttpClient());
+            var steamInterface = webInterfaceFactory.CreateSteamStoreInterface(new HttpClient());
             Task.Delay(3000).Wait();
-            var friendsListResponse = await steamInterface.GetAppListAsync();
-            var friendsList = friendsListResponse.Data;
-
-            return friendsList.ToList();
+            var game = await steamInterface.GetStoreAppDetailsAsync(346110);
+            return game;
         }
     }
 }
